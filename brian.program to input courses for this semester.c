7fce3b58a31345bb951f99a03ea37b4874fd2bc1
Brian.M <bwire971@gmail.com>
2025-09-22 11:21:40 +0300

    Add files via upload
    assignment to display units registered for year one semester one

---
+++ brian.program to input courses for this semester.c
@@ -0,0 +1,22 @@
+//Program to display the units registered for this semester
+/*
+Name:Brian Masakhwe
+REG No:PA106/G/28765/25
+Description:Program to display the units registered for this semester
+*/
+
+#include <stdio.h>
+int main(){
+	
+	printf("Mathematics for science\n");
+	printf("Introduction to programming and problem solving\n");
+	printf("Descrete mathematics\n");
+	printf("Communiucation and literacy skills\n");
+	printf("System analysis and design\n");
+	printf("Fundamentals of ICT\n");
+	printf("Computer systems and organization\n");
+	
+		
+	return 0;
+}
+	
