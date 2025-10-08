10d94db2638adae64e9acee514401874a9ad7c56
Brian_M <bwire971@gmail.com>
2025-10-08 20:04:36 +0300

    Add files via upload

---
+++ numbers _in_descending_order.c
@@ -0,0 +1,17 @@
+//program to print numbers 50-100 in descending order 
+/*
+Name: Brian Masakhwe 
+REG NO:PA106/G/28765/25
+Description : Numbers 50 to 100 in descending order 
+*/
+
+#include<stdio.h>
+int main (){
+
+int i=100;
+
+for (i=100; i>=50;i--)
+printf("%d\n",i);
+
+return 0;
+}
