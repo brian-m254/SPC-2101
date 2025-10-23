004e9357dcbe30d59e0852e125858c87f3f80db8
Brian_M <bwire971@gmail.com>
2025-10-23 12:25:31 +0300

    Add files via upload

---
+++ electric_bill_calculator.c
@@ -0,0 +1,37 @@
+//C function to calculate electric bill
+/*
+Name:Brian Masakhwe
+REG NO:PA106/G/28765/25
+*/
+
+#include <stdio.h>
+
+int total_electric_bill(int units,int amount);
+
+int main()
+{
+	int units,amount;
+	
+	printf("enter the amount of units consumed: ");
+	scanf("%d",&units);
+	
+	if(units<=100){
+		amount=units*10;
+	}
+	else if(units>100 &&units<=200){
+	amount=units*15;
+	}
+	else if(units>200){
+	amount=units*20;	
+	}
+	
+	printf("total_electric_bill= %d\n",amount);
+	return 0;
+}
+
+int total_electric_bill(int units,int amount)
+{
+	int result;
+	result=amount;
+	return result;
+}
